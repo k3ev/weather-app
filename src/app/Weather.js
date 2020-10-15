aa09b73d62264209d5f425c7a2435c973817b40d
Kevin Chacon <kevinchacong95@gmail.com>
2020-10-15 00:14:24 -0300

    Adding data to the UI

--- src/app/Weather.js
+++ src/app/Weather.js
@@ -7,7 +7,7 @@ export class Weather {
     async getWeather(){
         const URI = `https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.countryCode}&appid=${this.apikey}&units=metric`
         const response = await fetch(URI)
-        const data = await response.json()
+        const data = response.json()
         return data
     }
 }
--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,15 @@
 require('./index.css')
 const { Weather } = require('./Weather')
+const { UI } = require('./UI')
+
 const weather = new Weather('London','uk')
+const ui = new UI()
 
 async function fetchWeather() {
     const data = await weather.getWeather()
     console.log(data)
+    console.log(data.main.temp)
+    ui.render(data)
 }
 
 document.addEventListener('DOMContentLoaded', fetchWeather)
